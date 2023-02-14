1a63eee0b183fd64e61e080f8d23f7bbbe75f590
git-user-cpp <akushyk799@gmail.com>
2023-02-14 09:04:10 +0200

    Colors was added

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ SOFTWARE.
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use colored::*;
 
 fn main() {
     
@@ -54,9 +55,9 @@ fn main() {
         println!("You guessed {guess}");
 
         match guess.cmp(&secret_number){
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {println!("You win"); break;},
+            Ordering::Less => println!("{}","Too small".red()),
+            Ordering::Greater => println!("{}", "Too big".red()),
+            Ordering::Equal => {println!("{}", "You win".green()); break;},
         }
     }
 }
